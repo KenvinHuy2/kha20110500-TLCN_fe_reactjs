d13e565cc2770a713b81406ea44e7ee01666105f
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2023-11-28 22:34:29 +0700

    Update header

--- src/pages/admin/orders/Orders.jsx
+++ src/pages/admin/orders/Orders.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Orders = () => {
   return (
     <>
-      <h2>Quản lý đơn hàng</h2>
+      <h2 className='pt-3 pl-3 m-0 text-uppercase'>Quản lý đơn hàng</h2>
+      <hr />
     </>
   );
 };
--- src/pages/admin/store/Store.jsx
+++ src/pages/admin/store/Store.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const Store = () => {
   return (
     <>
-      <h2>Quản lý cửa hàng</h2>
+      <h2 className='pt-3 pl-3 m-0 text-uppercase'>Quản lý cửa hàng</h2>
       <hr />
       <Outlet />
     </>
